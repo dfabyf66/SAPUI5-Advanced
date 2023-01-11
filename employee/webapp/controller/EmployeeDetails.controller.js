e50f9a7c24e89bf560b579a98b241030fb599aee
dfabyf66 <diegofabianf@yahoo.com>
2023-01-11 14:43:32 +0000

    07_Bindings_Formatters

--- employee/webapp/controller/EmployeeDetails.controller.js
+++ employee/webapp/controller/EmployeeDetails.controller.js
@@ -1,11 +1,54 @@
+// @ts-nocheck
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+   //"logaligroup/Employees/model/formatter"
 ], function (Controller) {
 
-    return Controller.extend("dffspaceEmp.employee.controller.EmployeeDetails", {
+    function onInit() {
 
-        onInit: function () {
+    };
 
-        }
-    });
-}); 
+    function onCreateIncidence() {
+
+        var tableIncidence = this.getView().byId("tableIncidence");
+        var newIncidence = sap.ui.xmlfragment("dffspaceEmp.employee.fragment.NewIncidence", this);
+        var incidenceModel = this.getView().getModel("incidenceModel");
+        var odata = incidenceModel.getData();
+        var index = odata.length;
+        odata.push({ index: index + 1 });
+        incidenceModel.refresh();
+        newIncidence.bindElement("incidenceModel>/" + index);
+        tableIncidence.addContent(newIncidence);
+
+    };
+
+    // function onDeleteIncidence(oEvent) {
+
+    //     var tableIncidence = this.getView().byId("tableIncidence");
+    //     var rowIncidence = oEvent.getSource().getParent().getParent();
+    //     var incidenceModel = this.getView().getModel("incidenceModel");
+    //     var odata = incidenceModel.getData();
+    //     var contextObj = rowIncidence.getBindingContext("incidenceModel");
+
+    //     odata.splice(contextObj.index - 1, 1);
+    //     for (var i in odata) {
+    //         odata[i].index = parseInt(i) + 1;
+    //     };
+
+    //     incidenceModel.refresh();
+    //     tableIncidence.removeContent(rowIncidence);
+
+    //     for (var j in tableIncidence.getContent()){
+    //         tableIncidence.getContent()[j].bindElement("incidenceModel>/"+j);
+    //     }
+    //     };
+
+        var EmployeeDetails = Controller.extend("dffspaceEmp.employee.controller.EmployeeDetails", {});
+
+        EmployeeDetails.prototype.onInit = onInit;
+        EmployeeDetails.prototype.onCreateIncidence = onCreateIncidence;
+       // EmployeeDetails.prototype.onDeleteIncidence = onDeleteIncidence;
+        // EmployeeDetails.prototype.Formatter = formatter;
+
+        return EmployeeDetails;
+    }); 
