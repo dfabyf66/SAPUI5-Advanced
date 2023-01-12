1e336725bc4399e99f848b03a56267d30d4526ca
dfabyf66 <diegofabianf@yahoo.com>
2023-01-12 15:50:30 +0000

    09_Validaciones_Mensajes

--- employee/webapp/controller/Main.controller.js
+++ employee/webapp/controller/Main.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
+], function (Controller, MessageBox) {
 
     return Controller.extend("dffspaceEmp.employee.controller.Main", {
 
@@ -94,7 +95,7 @@ sap.ui.define([
                 this.getView().getModel("incidenceModel").create("/IncidentsSet", body, {
                     success: function () {
                         this.onReadODataIncidence.bind(this)(employeeId);
-                        sap.m.MessageToast.show(oResourceBundle.getText("odataSaveOK"));
+                        MessageBox.success(oResourceBundle.getText("odataSaveOK"));
                     }.bind(this),
                     error: function (e) {
                         sap.m.MessageToast.show(oResourceBundle.getText("odataSaveKO"));
@@ -145,6 +146,10 @@ sap.ui.define([
                     tableIncidence.removeAllContent();
 
                     for (var incidence in data.results) {
+
+                        data.results[incidence]._ValidateDate = true;
+                        data.results[incidence].EnabledSave = false;
+
                         var newIncidence = sap.ui.xmlfragment("dffspaceEmp.employee.fragment.NewIncidence",
                             this._detailEmployeeView.getController());
                         this._detailEmployeeView.addDependent(newIncidence);
