8a4265d2e56bc9061bcb371f355dcc99934e9a5e
Diego Fabian Ferrer <diegofabianf@yahoo.com>
2023-01-16 17:39:04 +0000

    08.1_OData_ONPREMISE

--- employee/webapp/Component.js
+++ employee/webapp/Component.js
@@ -29,7 +29,11 @@ sap.ui.define([
 
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
-            }
+            },
+
+            SapId : "diegofabianf@yahoo.com"
+
+
         });
     }
 );
